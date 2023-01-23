1840bc96cbdc9fbb55e1dcacb2de5bd422394abb
NathParnell <parnelln01@gmail.com>
2023-01-23 09:35:47 +0000

    Add project files.

---
+++ ElectionTracker/Classes/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionTracker.Classes
+{
+    public class Person
+    {
+        public Person(string Name, string Email)
+        {
+            this.Name = Name;
+            this.Email = Email;
+        }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        
+    }
+}
---
+++ ElectionTracker/Classes/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionTracker.Classes
+{
+    public class User : Person
+    {
+        public User (string Name, string Email)
+        {
+        }
+    }
+}
